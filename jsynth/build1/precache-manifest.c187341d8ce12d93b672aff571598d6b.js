433073825a7e51d7d59437a9fcc8c444832d64ba
Jade Rose <moosella44@gmail.com>
2020-07-04 20:21:20 -0400

    build

---
+++ jsynth/build1/precache-manifest.c187341d8ce12d93b672aff571598d6b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd9e448cf2afd0e4dd29b4c82227c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de0cfd46ca2a1ca7adc8",
+    "url": "/static/css/main.82228784.chunk.css"
+  },
+  {
+    "revision": "6aa954f98a811ae899ef",
+    "url": "/static/js/2.4e9a2fcf.chunk.js"
+  },
+  {
+    "revision": "49ed4fe4e031c7461ef3b8d47b4c80c4",
+    "url": "/static/js/2.4e9a2fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0cfd46ca2a1ca7adc8",
+    "url": "/static/js/main.671e854c.chunk.js"
+  },
+  {
+    "revision": "c098caed033b092e1997",
+    "url": "/static/js/runtime-main.94245e7c.js"
+  },
+  {
+    "revision": "2a549a99d9f1c57092642b558e7ccdc6",
+    "url": "/static/media/Eidolon.2a549a99.mp3"
+  },
+  {
+    "revision": "1b63c2f4ad324415680d00bbcb368b95",
+    "url": "/static/media/Grainboi.1b63c2f4.png"
+  },
+  {
+    "revision": "017f5086fdad70d6e6a9bd3fa6892be7",
+    "url": "/static/media/NASA_RadioTelescope.017f5086.mp3"
+  },
+  {
+    "revision": "c8fec853285c1c6b4ee9de2eca38bc3d",
+    "url": "/static/media/SciDrum.c8fec853.mp3"
+  },
+  {
+    "revision": "e7f41580c26ee762f0a70c96c37181ec",
+    "url": "/static/media/boot.e7f41580.mp3"
+  }
+]);
